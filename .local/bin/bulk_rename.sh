a0efa461cfb025da150f3d0eeeb6b02d4ade5916
Isaac Morneau <isaacmorneau@gmail.com>
2019-12-13 02:42:22 -0800

    made it simpler for lots of files

--- .local/bin/bulk_rename.sh
+++ .local/bin/bulk_rename.sh
@@ -11,21 +11,13 @@ newlisting=$(<"${file}")
 orig=( ${listing} )
 new=( ${newlisting} )
 
-#while IFS= read -r line; do
-#    orig+=("${line}")
-#done <<< "${listing}"
-#
-#while IFS= read -r line; do
-#    new+=("${line}")
-#done <<< "${listing}"
-
 for i in "${!orig[@]}"; do
     a="${orig[${i}]}"
     b="${new[${i}]}"
     if [ "${a}" = "${b}" ]; then
         continue
     fi
-    echo "mv '${a}' '${b}'"
+    mv "${a}" "${b}"
 done
 
-echo "rm '${file}'"
+rm "${file}"
